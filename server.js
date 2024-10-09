05cbd4829f94cc02880fd4a36b9f2dac5076b859
Nalhab <nathanlrbn@gmail.com>
2024-10-09 18:27:04 +0200

    test

--- server.js
+++ server.js
@@ -4,10 +4,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir les fichiers statiques du dossier public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Lancer le serveur
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
