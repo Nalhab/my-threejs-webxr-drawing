d0d8b287b7c64f630e8b58886124dc88698bef05
Nalhab <nathanlrbn@gmail.com>
2024-10-17 14:55:16 +0200

    new architecture

--- scripts/index.js
+++
@@ -1,4 +0,0 @@
-document.getElementById('actionButton').addEventListener('click', function() {
-    this.style.backgroundColor = 'lightblue';
-    console.log('Le bouton a été cliqué et la couleur a été changée !');
-});
